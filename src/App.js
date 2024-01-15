d4610963badbd89d17bcc1d3525af7e2e1d87a08
leiocopio <107382632+leiocopio@users.noreply.github.com>
2024-01-15 20:24:52 +0800

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+//Pages
+import Home from './pages/home'
+import About from './pages/about'
+import Contacts from './pages/contacts'
+import Footer from './pages/footer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+<>
+   <Router>
+<div id="body">
+
+      {/* Header or Navbar */}
+       <div id='navbar' className='p-5 flex flex-row justify-between items-center h-[4.375rem] px-20'>
+        
+        <div id="brandName">
+          <h2 className='text-2xl font-bold text-accent-400'> lei <span className='text-primary-400'>ocopio</span> </h2>
+        </div>
+
+        <div id="nav">
+          <center>
+          <ul className='flex flex-row gap-10'>
+
+           <li>
+             <Link to="/" className='font-bold hover:bg-primary-300 p-3 rounded-lg active:bg-primary-400 focus:bg-primary-500'>Home</Link>
+           </li>
+
+           <li>
+             <Link to="/about" className='font-bold hover:bg-primary-300 p-3 rounded-lg active:bg-primary-400 focus:bg-primary-500'>About</Link>
+           </li>
+
+           <li>
+             <Link to="/contacts" className='font-bold hover:bg-primary-300 p-3 rounded-lg active:bg-primary-400 focus:bg-primary-500'>Contact</Link>
+           </li>
+
+         </ul>
+         </center>
+
+        </div>
+
+        <div id="socials">
+          
+        </div>
+
+       </div>
+
+
+      {/* The main thing */}
+
+      <main className='mx-[10vh] mt-[5rem]'>
+        <Routes >
+
+         <Route path='/' element={<Home/>} />
+
+         <Route path='/about' element={<About/>} />
+
+         <Route path='/contacts' element={<Contacts/>} />
+
+       </Routes>
+
+      </main>
+
+
+       <div id="footer">
+          <Footer></Footer>
+       </div>
+    
+
     </div>
-  );
+   </Router>
+
+</>
+ );
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+function Home(){
+    return(
+<>
+<div id="homeMain">
+    <center>
+    <h1 className="font-bold text-5xl">
+    Hi! I'm Lei Ocopio
+    </h1>
+    </center>
+
+</div>
+
+</>
+    )
+}
+
+export default Home;
